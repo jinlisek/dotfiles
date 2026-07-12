c075672eeba14b9be92a67b25b65714d684c0520
Mariusz Mickiewicz <tastyramen@gmail.com>
2026-07-12 22:29:36 +0200

    install antidote and starshi

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ GREEN='\033[0m\033[32m'
 BLUE='\033[0m\033[34m'
 NC='\033[0m' # No Color
 
-echo -e "${BLUE}=== Installing dotfiles ===${NC}"
+echo -e "${BLUE}Installing dotfiles${NC}"
 
 DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo $DOTFILES_DIR
@@ -28,7 +28,13 @@ link_file() {
 mkdir -p "$HOME/.config"
 
 link_file "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
+
+echo -e "${BLUE}Installing starship${NC}"
+VERSION=v1.26.0 curl -sS https://starship.rs/install.sh | sh
+
+echo -e "${BLUE}Installing antidote${NC}"
+git clone --depth=1 --branch v2.1.0 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-$HOME}/.antidote
 link_file "$DOTFILES_DIR/.zsh_plugins.txt" "$HOME/.zsh_plugins.txt"
 link_file "$DOTFILES_DIR/starship.toml" "$HOME/.config/starship.toml"
 
-echo -e "${GREEN}=== Installation successful ===${NC}"
+echo -e "${GREEN}Finished installing dotfiles${NC}"
